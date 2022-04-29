51fed1ff7f676219e61776809e84d5345fa81dcb
jennasaikaly <jen.saikaly@gmail.com>
2022-04-29 14:50:49 -0400

    added code for player's health methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,5 +1,7 @@
 const Player = require('../lib/Player');
+const Potion = require('../lib/Potion.js');
 
+jest.mock('../lib/Potion.js');
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -31,4 +33,23 @@ test('creates a player object', () => {
     player.inventory = [];
   
     expect(player.getInventory()).toEqual(false);
+  });
+
+  test("gets player's health value", () => {
+    const player = new Player('Dave');
+  
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+  });
+
+  test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+  
+    player.reduceHealth(5);
+  
+    expect(player.health).toBe(oldHealth - 5);
+  
+    player.reduceHealth(99999);
+  
+    expect(player.health).toBe(0);
   });
